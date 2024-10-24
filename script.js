de11672cfd632e6c6414e4e2b8ae9e0b719af83e
AxeliDavin <axelius.davin@gmail.com>
2024-10-24 11:42:20 +0700

    add question10

--- script.js
+++ script.js
@@ -16,8 +16,15 @@ const canvas = document.getElementById('drawingCanvas');
 const clearCanvasButton = document.getElementById('clearCanvas');
 const ctx = canvas.getContext('2d');
 const canvasSubmit = document.getElementById("canvasSubmit");
+const canvas1 = document.getElementById('drawingCanvas1');
+const canvas2 = document.getElementById('drawingCanvas2');
+const textgambar1 = document.getElementById('selectCanvas1');
+const textgambar2 = document.getElementById('selectCanvas2');
+const ctx1 = canvas1.getContext('2d');
+const ctx2 = canvas2.getContext('2d');
+const doublecanvas = document.getElementById('canvas-container-two');
 
-let isDrawing = false;
+let isDrawing = false;  
 let totalsoal = 0;
 let totalbenar = 0;
 let totalsalah = 0;
@@ -56,6 +63,21 @@ function update(location) {
     canvasSubmit.style.display = 'block';
     canvasSubmit.innerText = location["button text"][0];
     canvasSubmit.onclick = location["button functions"][0];
+  } else if(location.canvasSelection) {
+    document.getElementById('canvas-container-two').style.display = 'block';
+    document.getElementById('drag-drop-container').style.display = 'none';
+    document.getElementById('canvas-container').style.display = 'none';
+    button1.style.display = button2.style.display = button3.style.display = button4.style.display = 'none';
+    canvas1.style.display = canvas2.style.display ='block';
+    textgambar1.style.display = 'block';
+    textgambar1.innerText = location["button text"][0];
+    textgambar1.onclick = location["button functions"][0];
+    textgambar2.style.display = 'block';
+    textgambar2.innerText = location["button text"][1];
+    textgambar2.onclick = location["button functions"][1];
+    if (location.preDrawnCanvas) {
+      loadPreDrawnImages();
+    }
   } else {
     document.getElementById('drag-drop-container').style.display = 'none';
     textseret.style.display = 'none';
@@ -105,6 +127,11 @@ function nextQuestion() {
     dropzone.style.display = 'none';
     textseret.style.display = 'none';
     canvasContainer.style.display = 'none';
+    canvas1.style.display = 'none';
+    canvas2.style.display = 'none';
+    textgambar1.style.display = 'none';
+    textgambar2.style.display = 'none';
+    doublecanvas.style.display ='none';
     button1.onclick = startQuiz;
     totalsoal = 0;
     totalbenar = 0;
@@ -173,6 +200,16 @@ const locations = [
     "button functions": [checkCanvasDrawing],
     dragAnswer: false,
     canvasQuestion: true,
+  },
+  {
+    name: "soal10",
+    text: "Pilih gambar lapangan baseball:",
+    "button text": ["Gambar 1", "Gambar 2", "", ""],
+    "button functions": [salah, benar, null, null],
+    dragAnswer: false,
+    canvasQuestion: false,
+    canvasSelection: true,
+    preDrawnCanvas: true,
   }
 ];
 
@@ -256,4 +293,62 @@ function clearCanvas() {
 function handleLogin(event) {
   event.preventDefault();
   window.location.href = 'pertanyaan.html';
+}
+
+canvas1.addEventListener('mousedown', (e) => {
+  isDrawingCanvas1 = true;
+  ctx1.beginPath();
+  ctx1.moveTo(e.offsetX, e.offsetY);
+});
+
+canvas1.addEventListener('mousemove', (e) => {
+  if (isDrawingCanvas1) {
+    ctx1.lineTo(e.offsetX, e.offsetY);
+    ctx1.stroke();
+  }
+});
+
+canvas1.addEventListener('mouseup', () => {
+  isDrawingCanvas1 = false;
+});
+
+canvas2.addEventListener('mousedown', (e) => {
+  isDrawingCanvas2 = true;
+  ctx2.beginPath();
+  ctx2.moveTo(e.offsetX, e.offsetY);
+});
+
+canvas2.addEventListener('mousemove', (e) => {
+  if (isDrawingCanvas2) {
+    ctx2.lineTo(e.offsetX, e.offsetY);
+    ctx2.stroke();
+  }
+});
+
+canvas2.addEventListener('mouseup', () => {
+  isDrawingCanvas2 = false;
+});
+
+function checkCanvasForDrawing(ctx) {
+  const canvasData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+  for (let i = 0; i < canvasData.data.length; i += 4) {
+    if (canvasData.data[i + 3] !== 0) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function loadPreDrawnImages() {
+  const img1 = new Image();
+  img1.src = 'futsal.jpg'; 
+  img1.onload = function() {
+    ctx1.drawImage(img1, 0, 0, canvas1.width, canvas1.height);
+  };
+
+  const img2 = new Image();
+  img2.src = 'baseball.png'; 
+  img2.onload = function() {
+    ctx2.drawImage(img2, 0, 0, canvas2.width, canvas2.height);
+  };
 }
